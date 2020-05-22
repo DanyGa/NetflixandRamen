ec033cee056f7a7363dc5fbd6e96e496061a9c6e
miriaampriscilaa@hotmail.com <miriaampriscilaa@hotmail.com>
2020-05-21 20:55:44 -0500

    OTRO

--- Netflix/Netflix/Form1.Designer.cs
+++ Netflix/Netflix/Form1.Designer.cs
@@ -31,7 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -52,16 +55,37 @@
             this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(43, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(265, 36);
+            this.label1.Size = new System.Drawing.Size(273, 36);
             this.label1.TabIndex = 1;
-            this.label1.Text = "DA CLIC EN LA IMAGEN PARA VER \r\nEL MEJOR DOCUMENTAL DE NETFLIX\r\n";
+            this.label1.Text = "DA CLIC EN ESTA IMAGEN PARA VER \r\nEL MEJOR DOCUMENTAL DE NETFLIX\r\n";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Netflix.Properties.Resources.YT;
+            this.pictureBox2.Location = new System.Drawing.Point(371, 107);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(166, 131);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "DA CLIC EN ESTA IMAGEN PARA VER\r\nEL MEJOR MV DE BTS";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(355, 283);
+            this.ClientSize = new System.Drawing.Size(607, 283);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
@@ -71,6 +95,7 @@
             this.ShowIcon = false;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +105,8 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
